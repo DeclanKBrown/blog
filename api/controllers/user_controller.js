41ebd599079fe86ecedb82a1931e2eabee0f03e6
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-10-16 20:57:54 +1100

    Created log in and out controller

--- api/controllers/user_controller.js
+++ api/controllers/user_controller.js
@@ -7,8 +7,42 @@ const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
 
-const { passwordCompare } = require('../utils/auth')
-const user = require('../models/user')
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username })
+
+      if (typeof user === undefined) {
+        return done(null, false, { message: 'Incorrect username' })
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password)
+      user.password = undefined
+
+      if (isPasswordMatch === false) {
+        return done(null, false, { message: 'Incorrect password' })
+      }
+
+      return done(null, user)
+    } catch (err) {
+      console.log('err', err)
+      return done(err)
+    }
+  })
+)
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id)
+    done(null, user)
+  } catch (err) {
+    done(err)
+  }
+})
 
 exports.sign_up = [
   body('username').trim().isLength({ min: 6, max: 12 }).escape(),
@@ -58,3 +92,38 @@ exports.sign_up = [
     }
   }),
 ]
+
+exports.log_in = asyncHandler(async (req, res, next) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: 'username and password are required.' })
+  }
+
+  passport.authenticate('local', function (err, user, info, status) {
+    if (err) {
+      return next(err)
+    }
+    if (!user) {
+      return res.status(status).json(info)
+    }
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1d' })
+
+    const updatedUser = user.toObject()
+
+    delete updatedUser.password
+
+    return res.status(201).json({ token, ...updatedUser })
+  })(req, res, next)
+})
+
+exports.log_out = asyncHandler(async (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err)
+    }
+    res.json({ message: 'successfully logged out' })
+  })
+})
--- api/utils/auth.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt'
-
-export const checkPassword = async (password, confirmPassword) => {
-  try {
-    return await bcrypt.compare(password, confirmPassword)
-  } catch (err) {
-    throw err
-  }
-}
